50ffadf400997b8ca7b53020c9559472c1844726
David Rivers <dmr41.rivers@gmail.com>
2015-03-23 10:56:16 -0600

    removed serverside sort

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -4,10 +4,12 @@ class RantsController < ApplicationController
   def index
     if params[:searchvale].present?
       @rants= Rant.where("title like ? OR body like ?", "%#{ params[:searchvale] }%", "%#{ params[:searchvale] }%")
-      @rants = @rants.order('created_at DESC')
+      # @rants = @rants.order('created_at DESC')
       render json: @rants
-    else
-      render json: Rant.order('created_at DESC')
+    # else
+    #   render json: Rant.order('created_at DESC')
+    end
+      render json: Rant.all
     end
   end
 
